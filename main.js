36cf919c72d8f764de0a92d764ee603e8489a7e2
Monet Carriere <MonetCarriere@yahoo.com>
2021-01-09 22:02:37 -0800

    getting practice with the DOM

---
+++ main.js
@@ -0,0 +1,115 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//QUERY SELECTOR
+
+/*const favBook = document.querySelector('#book-list li:nth-child(3) .name')
+
+//console.log(favBook)
+
+var books = document.querySelector('#book-list li .name')
+
+
+
+books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book){
+    console.log(book)
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const body = document.body;
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+//spanHi.removeAttribute(id);
+
+//spanHi.dataset.newName ="sup";
+
+//console.log(spanHi.dataset);
+
+spanHi.classList.remove("kanye")*/
+
+
